1c5a74a52adca8346cf041d77cb9804e67c48dd3
jimmylaskera <38056214+jimmylaskera@users.noreply.github.com>
2019-10-08 15:50:58 -0300

    Possivelmente concluído, ainda resta testar

--- src/br/com/lp2/roteador/Pacote.java
+++ src/br/com/lp2/roteador/Pacote.java
@@ -1,8 +1,8 @@
 package br.com.lp2.roteador;
 
 public class Pacote {
-    private int cabecalho;
-    private int terminador;
+    private int cabecalho = 0;
+    private int terminador = 0;
     private String dados;
     private String destino;
 
@@ -14,12 +14,24 @@ public class Pacote {
         this.destino = destino;
     }
 
+    public String getDados() { return dados; }
 
-    public String getDados() {
-        return dados;
-    }
-
-    public void setDados(String dados) {
-        this.dados = dados;
+    public void setDados(String dados) { this.dados = dados; }
+    
+    public String getDestino() { return destino; }
+    
+    public void setDestino(String destino) { this.destino = destino; }
+    
+    public boolean isStart() { return cabecalho == 1; }
+    
+    public boolean isEnd() { return terminador == 1; }
+    
+    public void setStart() { this.cabecalho = 1; }
+    
+    public void setEnd() { this.terminador = 1; }
+    
+    public void reset() {
+        cabecalho = 0;
+        terminador = 0;
     }
 }
